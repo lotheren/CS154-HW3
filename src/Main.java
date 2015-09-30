7220520a7e5b55120f2ee9dcffe655b289bbc1be
Lotheren <lotheren@gmail.com>
2015-09-29 19:07:08 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+      System.out.println("Hello and Welcome to MinDFA/TrimDFA");
+        System.out.println("Please input your DFA table");
+
+
+
+    }
+}
