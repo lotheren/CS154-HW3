ab7eb2bb33d737c00d252553b67551b3c82a74c4
Lotheren <lotheren@gmail.com>
2015-09-29 19:17:37 -0700

    Change 1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-      System.out.println("Hello and Welcome to MinDFA/TrimDFA");
-        System.out.println("Please input your DFA table");
+        System.out.println("Hello and Welcome to MinDFA/TrimDFA");
+        System.out.println("Please enter your DFA table in the following steps: ");
+        System.out.println("")
 
 
 
